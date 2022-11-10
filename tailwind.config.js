74b43ff6e53e168e64a03fccaa20184cc902b648
J. Vitor F <joaovitor.jobs2001@gmail.com>
2022-11-10 17:42:22 -0300

    feat: add font-family and color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,24 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: 'Poppins, sans-serif',
+    },
+    colors: {
+      white: '#fff',
+      green: {
+        50: '#bafff0',
+        300: '#00DFAF',
+        400: '#00C298',
+        900: '#005442',
+      },
+      gray: {
+        200: '#E2E8F0',
+        700: '#3D4751',
+        800: '#292E38',
+        900: '#1A1E23',
+      },
+    },
   },
   plugins: [],
 }
