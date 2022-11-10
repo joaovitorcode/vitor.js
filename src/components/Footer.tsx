ffb41d0f101495e8d5e675a0dce0bc999583dc7d
J. Vitor F <joaovitor.jobs2001@gmail.com>
2022-11-10 17:52:47 -0300

    feat: footer component done

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="w-full h-[72px] bg-gray-900 flex justify-center items-center text-white text-sm">
+      © 2022 vitor.js. Todos os direitos reservados.
+    </footer>
+  )
+}
