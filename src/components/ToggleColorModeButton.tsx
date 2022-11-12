7a5285ee39378ca9202eb335bf83e00188c78416
J. Vitor F <joaovitor.jobs2001@gmail.com>
2022-11-12 11:55:09 -0300

    feat: add toggle color mode button component

---
+++ src/components/ToggleColorModeButton.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+import { MdDarkMode, MdLightMode } from 'react-icons/md'
+
+export const ToggleColorModeButton = () => {
+  const [isDark, setIsDark] = useState(false)
+
+  useEffect(() => {
+    const htmlElement = document.querySelector('html')
+    isDark
+      ? htmlElement?.classList.add('dark')
+      : htmlElement?.classList.remove('dark')
+  }, [isDark])
+
+  return (
+    <button
+      onClick={() => setIsDark(prevState => !prevState)}
+      className="text-xl sm:text-2xl text-gray-900 dark:text-white hover:text-green-400 dark:hover:text-green-400"
+    >
+      {isDark ? (
+        <MdLightMode title="Mode light" />
+      ) : (
+        <MdDarkMode title="Modo dark" />
+      )}
+    </button>
+  )
+}
