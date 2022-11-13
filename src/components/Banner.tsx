2b4ec3df6b6a7f2262191f9c8aa2d4463d101d5a
J. Vitor F <joaovitor.jobs2001@gmail.com>
2022-11-13 11:17:52 -0300

    refactor: improved accessibility

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,6 @@
 export const Banner = () => {
   return (
-    <div className="uppercase text-5xl sm:text-[96px] font-bold text-gray-900 dark:text-white flex justify-center">
+    <div className="uppercase text-5xl pb-12 sm:text-[96px] font-bold text-gray-900 dark:text-white flex justify-center">
       vitor.js
     </div>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,57 +7,52 @@ export const Header = () => {
   return (
     <header className="sticky top-0 z-10 w-full p-4 sm:p-6 bg-white dark:bg-gray-900 flex justify-between">
       <Link href="/">
-        <Image
-          src="/logo.svg"
-          alt="Logo do blog vitor.js"
-          width={107}
-          height={39}
-        />
+        <div className="relative w-[80px] sm:w-[107px] h-[29px] sm:h-[39px]">
+          <Image src="/logo.svg" alt="Logo do blog vitor.js" fill />
+        </div>
       </Link>
       <div className="flex items-center">
-        <ul className="flex gap-4 sm:gap-6">
-          <li className="flex items-center">
-            <Link
-              className="text-sm sm:text-base text-gray-900 dark:text-white uppercase hover:text-green-400 dark:hover:text-green-400"
-              href="/about"
-            >
-              About
-            </Link>
-          </li>
-          <li className="flex items-center">
-            <Link
-              className="text-sm sm:text-base text-gray-900 dark:text-white uppercase hover:text-green-400 dark:hover:text-green-400"
-              href="/skills"
-            >
-              Skills
-            </Link>
-          </li>
-          <span className="border-l border-gray-200 dark:border-gray-700" />
-          <li className="flex items-center">
-            <a
-              className="text-xl sm:text-2xl text-gray-900 dark:text-white hover:text-green-400 dark:hover:text-green-400"
-              href="https://github.com/joaovitorcode"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FaGithub title="GitHub" />
-            </a>
-          </li>
-          <li className="flex items-center">
-            <a
-              className="text-xl sm:text-2xl text-gray-900 dark:text-white hover:text-green-400 dark:hover:text-green-400"
-              href="https://www.linkedin.com/in/joaovitorcode/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FaLinkedin title="LinkedIn" />
-            </a>
-          </li>
-          <span className="border-l border-gray-200 dark:border-gray-700" />
-          <li className="flex items-center">
-            <ToggleColorModeButton />
-          </li>
-        </ul>
+        <nav>
+          <ul className="flex gap-4 sm:gap-6">
+            <li className="flex items-center">
+              <Link
+                className="text-sm sm:text-base text-gray-900 dark:text-white uppercase hover:text-green-400 dark:hover:text-green-400"
+                href="/about"
+              >
+                About
+              </Link>
+            </li>
+            <li className="flex items-center">
+              <Link
+                className="text-sm sm:text-base text-gray-900 dark:text-white uppercase hover:text-green-400 dark:hover:text-green-400"
+                href="/skills"
+              >
+                Skills
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex gap-4 sm:gap-6 pl-4 sm:pl-6 ml-4 sm:ml-6 border-l border-gray-200 dark:border-gray-700">
+          <a
+            className="text-xl sm:text-2xl text-gray-900 dark:text-white hover:text-green-400 dark:hover:text-green-400"
+            href="https://github.com/joaovitorcode"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub title="GitHub" />
+          </a>
+          <a
+            className="text-xl sm:text-2xl text-gray-900 dark:text-white hover:text-green-400 dark:hover:text-green-400"
+            href="https://www.linkedin.com/in/joaovitorcode/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedin title="LinkedIn" />
+          </a>
+        </div>
+        <div className="flex gap-4 sm:gap-6 pl-4 sm:pl-6 ml-4 sm:ml-6 border-l border-gray-200 dark:border-gray-700">
+          <ToggleColorModeButton />
+        </div>
       </div>
     </header>
   )
