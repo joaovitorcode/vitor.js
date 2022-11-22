f82512d64dc6a5205bdd48fc4e1787557ed7e37e
J. Vitor F <joaovitor.jobs2001@gmail.com>
2022-11-22 09:22:01 -0300

    feat: add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/global.css'
 import type { AppProps } from 'next/app'
 import { DarkModeContextProvider } from '../contexts/DarkModeContext'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <DarkModeContextProvider>
       <Component {...pageProps} />
+      <Analytics />
     </DarkModeContextProvider>
   )
 }
